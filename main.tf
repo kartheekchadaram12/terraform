6d0d90d5e133463adb8ccea9d8856b6bd5c2a735
kartheekchadaram12 <kartheekchadaram12@gmail.com>
2024-10-04 18:39:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "AP-SOUTH-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-00f251754ac5da7f0"
+  ami           = "ami-078264b8ba71bc45e"
   instance_type = "t2.micro"
 
   tags = {
